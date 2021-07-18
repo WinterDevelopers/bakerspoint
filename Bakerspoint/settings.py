34fe92818a0124c2c82a62f3070b65c5569e1e80
unknown <chrisprogram171041@gmail.com>
2021-07-18 09:41:46 +0100

    nav bar mobile editted

--- Bakerspoint/settings.py
+++ Bakerspoint/settings.py
@@ -30,7 +30,7 @@ SECRET_KEY = '$fo!t=8()z%ctfr^e-$rs_&v&a7j!a^m2@=#ml0u2az=6%d=i&'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['bakerspoint.herokuapp.com']
+ALLOWED_HOSTS = ['bakerspoint.herokuapp.com', '127.0.0.1']
 
 
 # Application definition
